b8715efcb112908406d676eb39c1c00093e0d74a
afraklaabdelaziz <afraklaabdelaziz@gmail.com>
2022-12-17 21:52:52 +0100

    install dependicies de projet

---
+++ src/main/java/com/example/visionarycroftingspring_security/VisionaryCroftingSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.visionarycroftingspring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionaryCroftingSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisionaryCroftingSpringSecurityApplication.class, args);
+    }
+
+}
